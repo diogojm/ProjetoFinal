c540f5d7e881cf01316a6ea5d18ea7b8a8eb8bb0
Diogo <diogojmdiogo@gmail.com>
2022-06-21 21:00:01 +0100

    Adicionada condição na introdução do nome

--- app/src/main/java/com/example/projetoprog/MainActivity.kt
+++ app/src/main/java/com/example/projetoprog/MainActivity.kt
@@ -2,15 +2,55 @@ package com.example.projetoprog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private var username: EditText? = null
+    private var textViewMensagem: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mainpage)
+        setContentView(R.layout.activity_main)
+
+        textViewMensagem = findViewById(R.id.textViewMensagem)
+        username = findViewById(R.id.username)
+
+        findViewById<Button>(R.id.buttonEntrar).setOnClickListener {
+            entrar()
+        }
 
     }
 
 
+
+
+    private fun entrar() {
+        val nome = username!!.text.toString().toIntOrNull()
+
+        if (nome !in NOME_MIN..NOME_MAX)
+            username!!.error = "O nome deve conter entre 3 e 10 caracteres"
+            username!!.requestFocus()
+        return
+
+
+        val mensagem = when {
+            nome!! < 3 -> "nome muito pequeno, introduza um nome entre 3 a 10 caracteres"
+            nome!! > 10 -> "nome muito grande, introduza um nome entre 3 a 10 caracteres"
+            else -> "Entrou com sucesso"
+        }
+
+        textViewMensagem!!.text = mensagem
+
+
+
+    }
+    companion object {
+        private const val NOME_MIN = 3
+        private const val NOME_MAX = 10
+    }
 
 }
